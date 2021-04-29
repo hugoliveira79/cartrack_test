48f513b15108f5b95245257e42677cdb0a5b0467
Hugo Figueiredo Oliveira <Hugo.Oliveira@isobar.com>
2021-04-29 14:48:30 +0100

    added table dump

---
+++ cars_202104291438.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.cars (model_name,model_type,model_brand,model_year,model_date_added,model_date_modified) VALUES
+	 ('Vios','Sedan','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Altis','Sedan','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Corolla','Sedan','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Mirage G4','Sedan','Mitsubishi','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Mirage HB','Hatchback','Mitsubishi','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('City','Sedan','Honda','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Civic','Sedan','Honda','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Fortuner','SUV','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Innova','Van','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Hilux','Truck','Toyota','2020','2021-02-01 00:16:19','2021-02-01 00:16:19');
+INSERT INTO public.cars (model_name,model_type,model_brand,model_year,model_date_added,model_date_modified) VALUES
+	 ('CRV','SUV','Honda','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Crosswind','SUV','Izuzu','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Spark','Sedan','Izuzu','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Accent','Sedan','Hyundai','2020','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('Veyron','sx','Bugatti','2015','2021-02-01 00:16:19','2021-02-01 00:16:19'),
+	 ('VW','Hatchback','Polo','2020','2021-03-31 16:47:53.855899','2021-03-31 16:47:53.855899'),
+	 ('Tesla','Coil','XPTO','2021','2021-04-07 13:02:03.910479','2021-04-07 13:02:03.910479'),
+	 ('Xpander','MPV','Mitsubishi','2021',NULL,'2021-04-13 09:37:18.24625'),
+	 ('Adventure','','','',NULL,'2021-04-13 09:39:48.248623'),
+	 ('Adventure','MPV','Mitsubishi','2021',NULL,'2021-04-13 11:35:41.115168');
+INSERT INTO public.cars (model_name,model_type,model_brand,model_year,model_date_added,model_date_modified) VALUES
+	 ('Senna2','Racing','McLarens','1993','2021-04-29 03:13:57','2021-04-29 03:25:52');
